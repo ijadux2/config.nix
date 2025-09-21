17f3d7569df2fca40c15e4cab23ecf4dd44c6bb2
kumar vaibhav <kv26102009@gmail.com>
2025-09-21 06:17:37 +0530

    added lib to onfig

--- nixos-config.nix/pkgs.nix
+++ nixos-config.nix/pkgs.nix
@@ -2,17 +2,165 @@
 
 {
     environment.systempackages = with pkgs; [
-    hyprland
-    hyprlock
+        # Hyprland Stuff
+    hypridle
+    hyprpolkitagent
+    pyprland
+    #uwsm
+    hyprlang
+    hyprshot
     hyprcursor
-    waybar
-    ghostty # extra terminal for experiment 
-    nwg-look
+    mesa
     nwg-displays
-    nitch  # fetch for nixos
-    networkmanager 
-    swww
+    nwg-look
+    waypaper
+    hyprland-qt-support # for hyprland-qt-support
+
+    #  Apps
+    loupe
+    appimage-run
+    bc
+    brightnessctl
+    (btop.override {
+      cudaSupport = true;
+      rocmSupport = true;
+    })
+    bottom
+    baobab
+    btrfs-progs
+    cmatrix
+    dua
+    duf
+    cava
+    cargo
+    clang
+    cmake
+    cliphist
+    cpufrequtils
+    curl
+    dysk
+    eog
+    eza
+    findutils
+    figlet
+    ffmpeg
+    fd
+    feh
+    file-roller
+    glib # for gsettings to work
+    gsettings-qt
+    git
+    google-chrome
+    gnome-system-monitor
     fastfetch
+    jq
+    gcc
+    git
+    gnumake
+    grim
+    grimblast
+    gtk-engine-murrine # for gtk themes
+    inxi
+    imagemagick
+    killall
+    kdePackages.qt6ct
+    kdePackages.qtwayland
+    kdePackages.qtstyleplugin-kvantum # kvantum
+    lazydocker
+    libappindicator
+    libnotify
+    libsForQt5.qtstyleplugin-kvantum # kvantum
+    libsForQt5.qt5ct
+    (mpv.override { scripts = [ mpvScripts.mpris ]; }) # with tray
+    nvtopPackages.full
+    openssl # required by Rainbow borders
+    pciutils
+    networkmanagerapplet
+    #nitrogen
+    #nvtopPackages.full
+    pamixer
+    pavucontrol
+    playerctl
+    #polkit
+    # polkit_gnome
+    kdePackages.polkit-kde-agent-1
+    # qt6ct
+    #qt6.qtwayland
+    #qt6Packages.qtstyleplugin-kvantum # kvantum
+    # gsettings-qt
+    rofi
+    slurp
+    swappy
+    swaynotificationcenter
+    swww
+    unzip
+    wallust
+    wdisplays
+    wl-clipboard
+    wlr-randr
+    wlogout
+    wget
+    xarchiver
+    yad
+    yazi
+    yt-dlp
+
+    (inputs.quickshell.packages.${pkgs.system}.default)
+    (inputs.ags.packages.${pkgs.system}.default)
+
+    # Utils
+    caligula # burn ISOs at cli FAST
+    atop
+    gdu
+    glances
+    gping
+    htop
+    hyfetch
+    ipfetch
+    lolcat
+    lsd
+    oh-my-posh
+    pfetch
+    ncdu
+    ncftp
+    ripgrep
+    socat
+    starship
+    tldr
+    ugrep
+    unrar
+    v4l-utils
+    obs-studio
+    zoxide
+
+    # Hardware related
+    cpufetch
+    cpuid
+    cpu-x
+    #gsmartcontrol
+    smartmontools
+    light
+    lm_sensors
+    mission-center
+    neofetch
+
+    # Development related
+    luarocks
+    nh
+
+    # Internet
+    discord
+
+    # Virtuaizaiton
+    virt-viewer
+    libvirt
+
+    # Video
+    vlc
+
+    # Terminals
+    kitty
+    wezterm
 
     ];
   }
