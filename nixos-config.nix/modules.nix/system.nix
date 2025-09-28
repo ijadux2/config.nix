f821699d40b99a7d0cc61dae46c2e717d020a825
kumar vaibhav <kv26102009@gmail.com>
2025-09-28 10:19:42 +0530

    Update system.nix

--- nixos-config.nix/modules.nix/system.nix
+++ nixos-config.nix/modules.nix/system.nix
@@ -1,31 +1,25 @@
 { config, pkgs, ... }:
 
 {
-    # boot loaders 
+ 
 boot.loader.systemd-boot.enable = true;
 boot.loader.efi.canTouchEfiVariables = true;
-# networking and connectivity
 networking.hostname = "nix-btw";
 networking.networkmanager.enable = true;
-#timeZone 
 time.timeZone = "Asia/Kolkata";
-service.printing.enble = false;
 service.displayManager.ly.enable = true;
 
-#audio and sound adjestments 
-service.pulseaudio.enable = false;
 service.pipewire.enable = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true; 
     pulse.enable = true;
     thisjack.enable = true; 
-  }
+  };
+
 security.rtkit.enableble = true;
-#some extra settings for pkgs and users 
 nixpkgs.config.allowUnfree.enable = true;
-# firefox enble
 programs.firefox.enable = true;
-# system version 
 systems.stateVersion = "25.05";
+
   }
